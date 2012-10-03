c84fe90315cb1a9c1fce6a43aca15b1797e88244
Vandana Bachani <vandana@yuan-Vostro-420-Series.(none)>
2012-10-02 19:32:31 -0500

    Updating crawler to use new data structures

--- src/crawler/base.py
+++ src/crawler/base.py
@@ -2,28 +2,38 @@
 Created on Sep 19, 2012
 
 @author: vandana
-Basic domain specific crawler
+Basic web crawler
 """
-from BeautifulSoup import BeautifulSoup
-from abc import abstractmethod 
-from collections import deque
+
+from abc import abstractmethod
+from src.common.data_structures import LUOQueue, NativeSet
 import httplib2
 
 class Crawler:
     """
+    A basic web crawler which starts off with a set of links.
     
+    It declares abstract methods for processing domain specific html
+    and extracting links and seed the crawler which need to be implemented by
+    application or domain specific derived crawlers.
     """
-    def __init__(self, start_links):
-        # try to keep this stuff in database.
-        # or load from database as this list might get bigger
-        self.unvisited_urls = deque();
-        self.unvisited_urls.extend(start_links)
-        # this can be a database table too.
-        self.visited_urls = [];
+    def __init__(self, start_links=None):
+        # look up optimized queue
+        if start_links:
+            self.unvisited_urls = LUOQueue(start_links)
+        else:
+            self.unvisited_urls = LUOQueue()
+        more_links = self.get_start_links()
+        self.unvisited_urls.extend(more_links)
+        print self.unvisited_urls
+        
+        # set of visited urls
+        # change to another implementation if needed
+        self.visited_urls = NativeSet()
         
         # make sure to arrange for caching if the above lists end up
         # being a database
-        self.current_link = None;        
+        self.current_link = None;       
     
     #can be a static method
     def crawl(self):
@@ -35,7 +45,7 @@ class Crawler:
         # can and makes sure it does the required memory management
         http = httplib2.Http()
         while self.unvisited_urls:
-            link = self.unvisited_urls.popleft()
+            link = self.unvisited_urls.dequeue()
             (header, pagehtml) = http.request(link, 'GET')
             # process header if needed especially for error response to slow
             # down or sleep
@@ -44,7 +54,7 @@ class Crawler:
             for i in urls:
                 if i in self.visited_urls or i in self.unvisited_urls:
                     continue
-                self.unvisited_urls(i)
+                self.unvisited_urls.enqueue(i)
         if not self.unvisited_urls:
             return ReturnValues.CRAWL_STOPPED_EMPTY
     
@@ -57,14 +67,18 @@ class Crawler:
         # any kind of spurious situations which occur need to be handled
         pass
     
-    @abstractmethod
     def get_start_links(self):
-        pass
+        """
+        Derived classes may implement to add more start links
+        """
+        return []
     
     @abstractmethod
     def process_html(self):
-        #extracts urls and processes the html according to requirements
-        pass
+        """
+        Extracts urls and processes the html according to requirements
+        """
+        raise NotImplementedError
          
 
 class ReturnValues:
