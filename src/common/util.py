c0f421bcd05edb0dc946e6efa9f6c44aeb29bec0
Vandana Bachani <vandana@yuan-Vostro-420-Series.(none)>
2012-10-31 18:14:28 -0500

    utility functions, added a function to get html of a given web url

---
+++ src/common/util.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 31, 2012
+
+@author: vandana
+'''
+import httplib2
+
+def get_contents(url):
+	http = httplib2.Http()
+	(header, pagehtml) = http.request(url, 'GET')
+	print header
+	if header['status'] == '200':
+		return pagehtml
+	else:
+		return None
+
+if __name__ == '__main__':
+	r = get_contents('http://google.com')
+	print r
