b319497db76057a7acf6c815437728f3f43e7f00
Vardharaj L <vadi1234@gmail.com>
2012-11-03 11:57:28 -0500

    Completed definition of process_html in user_crawler.py

--- src/pinterest/user_crawler.py
+++ src/pinterest/user_crawler.py
@@ -5,7 +5,7 @@ Created on Sep 19, 2012
 Module for crawling pinterest.com user data.
 '''
 
-from crawler.base import Crawler
+from src.crawler.base import Crawler
 from bs4 import BeautifulSoup
 from src.common import util
 import httplib2
@@ -44,32 +44,62 @@ class UserCrawler(Crawler):
     
     def process_html(self, html):
         """
+        Given a user page html, this function will get all the information of the user
+        - Number of Boards, Pins, Likes, Followers, Following, Location (if specified)
+        Also, it will construct the user's following page URL, get it's HTML and find 
+        who all this user is following and will pass them to the crawler.
         """
         # we don't want to save the entire content - not very useful.
+        print "Trace : Processing html...\n"
         bs = BeautifulSoup(html)
         bs.prettify()
-        links = []
-        # process the page for complete user profile, all attributes etc
-        # the board/pin urls
-        # stats
-        # followers and following network
-        # page1 = html
-        # page2 = get userid from html generate URL for userid/following
-        # crawl page2 to get userids of persons the user is following div class="person", href = "/vistabill"
-        # add new URLs to links
+        links = []        
+        user_data = {}
+        
         # populate user data structure
+        #get location
+        location = bs.find('li', attrs={'id' : 'ProfileLocation'})
+        if location != None:
+        	loc = str(location.contents[2])
+        	user_data['location'] = loc.strip()      
         
-        location = bs.find_all('span', attrs={'class' : 'icon location'})  
-        #dynamic web pages?
-        #location.content has the profile location
+        #collecting user data number of boards, pins, likes, followers, following
+		#boards, pins, likes, followers, following
+		
+        bpl =  bs.find('ul', attrs={'class' : 'links'})
+        atags = bpl.findAll('a')
+		#print atags
         
-        li_tag = bs.find_all('li');
-        a_tag = li_tag.find_all('a', attrs={'class' : 'selected'});
-        tuples = a_tag[0].attrs
-        for t in tuples:
-        	if t == "href":
-            	page1.append(self.base_url + tuples[t] + "following")
-        	
+        for atag in atags:
+			if(len(atag.contents) > 2):
+				num = atag.contents[1].contents[0]
+				ntypes = str(atag.contents[2])
+				user_data[ntypes.strip()] = str(num)
+		
+    	ff = bs.find('ul', attrs={'class' : 'follow'})
+    	atags = ff.findAll('a')
+    	for atag in atags:
+			if(len(atag.contents) > 2):
+				num = atag.contents[1].contents[0]
+				ntypes = str(atag.contents[2])
+				user_data[ntypes.strip()] = str(num)
+        print user_data
+        
+        # user_data - dictionary DS with user data ,need to pass it to get_json
+        # constructing 'following' link
+        
+        a_tag = bs.find_all('a', attrs={'class' : 'selected'});
+        for a in a_tag:        	
+        	page = (self.base_url + a['href'] + "following")
+        
+        html1 = get_contents(page)
+        if (html1 != None):
+        	bs_follow = BeautifulSoup(html1)
+        	divs = bs_follow.find_all('div', attrs={'class' : 'person'})
+        	for div in divs:
+        		atag = div.find('a')
+        		links.append(self.base_url + atag['href'])
+        		
         return links
 
 def main(argv=None):
