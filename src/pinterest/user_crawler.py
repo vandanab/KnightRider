5fc2fe7e5b2063ad3ba784e469d1e169084f9dd2
Vardharaj L <vadi1234@gmail.com>
2012-11-21 16:54:48 -0600

    Completed user_crawler for time being, need to modularize and make some small changes

--- src/pinterest/user_crawler.py
+++ src/pinterest/user_crawler.py
@@ -10,6 +10,10 @@ from bs4 import BeautifulSoup
 from src.common import util
 import httplib2
 import sys 
+import cjson
+
+PINS_FILE = "/media/08A7089D4E9B9DB1/study/TAMU/pinterest/KnightRider/src/pinterest/pins.json"
+USER_FILE = "/media/08A7089D4E9B9DB1/study/TAMU/pinterest/KnightRider/src/pinterest/users.json"
 
 class UserCrawler(Crawler):
     def __init__(self):
@@ -41,7 +45,12 @@ class UserCrawler(Crawler):
                     if t == "href":
                         links.append(self.base_url + tuples[t])
         return links;
-    
+        
+    def write_to_file_json(self, file_name, data):
+    	f = open(file_name, 'a')
+    	f.write(cjson.encode(data))
+    	f.close()
+    	
     def process_html(self, html):
         """
         Given a user page html, this function will get all the information of the user
@@ -55,51 +64,91 @@ class UserCrawler(Crawler):
         bs.prettify()
         links = []        
         user_data = {}
+        pin_data = {}
+        fail_count = 0
         
+        def get_count(class_id):
+            count = pin.find(class_=class_id)
+            if count:
+                count_split = count.get_text().strip().split()
+                if count_split: return int(count_split[0])
+                else: return 0
+            else: return 0
+            
         # populate user data structure
         #get location
-        location = bs.find('li', attrs={'id' : 'ProfileLocation'})
-        if location != None:
-        	loc = str(location.contents[2])
-        	user_data['location'] = loc.strip()      
-        
-        #collecting user data number of boards, pins, likes, followers, following
-		#boards, pins, likes, followers, following
+        while fail_count < 10:
+				user_id = bs.find(class_= "selected").get('href')
+				user_data['user_id'] = user_id
+				location = bs.find('li', attrs={'id' : 'ProfileLocation'})
+				if location != None:
+					#print location.contents[2]
+					#loc = str(location.contents[2])
+					user_data['location'] = location.contents[2]      
+				
+				#collecting user data number of boards, pins, likes, followers, following
+				#boards, pins, likes, followers, following
+				bpl =  bs.find('ul', attrs={'class' : 'links'})
+				if bpl != None:
+					atags = bpl.findAll('a')
+					for atag in atags:
+						if(len(atag.contents) > 2):
+							num = atag.contents[1].contents[0]
+							ntypes = str(atag.contents[2])
+							user_data[ntypes.strip()] = str(num)
 		
-        bpl =  bs.find('ul', attrs={'class' : 'links'})
-        atags = bpl.findAll('a')
-		#print atags
-        
-        for atag in atags:
-			if(len(atag.contents) > 2):
-				num = atag.contents[1].contents[0]
-				ntypes = str(atag.contents[2])
-				user_data[ntypes.strip()] = str(num)
-		
-    	ff = bs.find('ul', attrs={'class' : 'follow'})
-    	atags = ff.findAll('a')
-    	for atag in atags:
-			if(len(atag.contents) > 2):
-				num = atag.contents[1].contents[0]
-				ntypes = str(atag.contents[2])
-				user_data[ntypes.strip()] = str(num)
-        print user_data
-        
-        # user_data - dictionary DS with user data ,need to pass it to get_json
-        # constructing 'following' link
-        
-        a_tag = bs.find_all('a', attrs={'class' : 'selected'});
-        for a in a_tag:        	
-        	page = (self.base_url + a['href'] + "following")
-        
-        html1 = util.get_contents(page)
-        if (html1 != None):
-        	bs_follow = BeautifulSoup(html1)
-        	divs = bs_follow.find_all('div', attrs={'class' : 'person'})
-        	for div in divs:
-        		atag = div.find('a')
-        		links.append(self.base_url + atag['href'])
-        		
+				ff = bs.find('ul', attrs={'class' : 'follow'})
+				if ff:
+					atags = ff.findAll('a')
+					for atag in atags:
+						if(len(atag.contents) > 2):
+							num = atag.contents[1].contents[0]
+							ntypes = str(atag.contents[2])
+							user_data[ntypes.strip()] = str(num)
+				user_data['boards'] = []
+			
+				# Go to the board URL and get all the pins of that board
+				boards = bs.find_all('div', attrs={'class':'board'})
+				for board in boards:
+					atag = board.find('a')
+					board_url = self.base_url + atag['href']
+					pin_data['user_id'] = user_id
+					pin_data['board-id'] = atag['href']
+					user_data['boards'].append(atag['href'])
+					html = util.get_contents(board_url)
+					if html != None:
+						bs = BeautifulSoup(html)
+						pins = bs.find_all('div', attrs={'class':'pin'})
+						for pin in pins:
+							pin_data['id'] = pin['data-id']
+							pin_data['description'] = pin.find(class_='description').get_text()
+							pin_data['Comments_count'] = get_count('CommentsCount')
+							pin_data['Likes_count'] = get_count('LikesCount')
+							pin_data['Repin_count'] = get_count('RepinsCount')
+							links = pin.find_all(class_ = 'convo attribution clearfix')
+							self.write_to_file_json(PINS_FILE, pin_data)     				
+							for link in links:
+								atag = link.find('a')
+								if atag != None:
+									pin_data['link'] = atag['href']
+						
+				self.write_to_file_json(USER_FILE, user_data)
+				# user_data - dictionary DS with user data ,need to pass it to get_json
+				# constructing 'following' link
+				
+				page = []
+				a_tag = bs.find_all('a', attrs={'class' : 'selected'});
+				for a in a_tag:        	
+					page = (self.base_url + a['href'] + "following")
+
+				html1 = util.get_contents(page)
+				if (html1 != None):
+					bs_follow = BeautifulSoup(html1)
+					divs = bs_follow.find_all('div', attrs={'class' : 'person'})
+					for div in divs:
+						atag = div.find('a')
+						links.append(self.base_url + atag['href'])
+				        		
         return links
 
 def main(argv=None):
